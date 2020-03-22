9d82662437147f412f7da5d9c53dcf80765ae8e8
MariuszSielicki <suchowryjumam@gmail.com>
2020-03-22 09:24:00 +0100

    added customized youtube-dl options (manually added) reorganized menu

--- main/ytd.bat
+++ main/ytd.bat
@@ -1,89 +1,116 @@
 ::v 1.0
 @ECHO OFF
-SET video=""
-SET audio="-x --audio-format mp3 --audio-quality 0"
+SET video="--no-overwrites --no-continue --restrict-filenames"
+SET audio="--no-overwrites --no-continue --restrict-filenames -x --audio-format mp3 --audio-quality 0"
 SET format=%video%
+
+::for /f "tokens=4 delims= " %%a in ('VER') do SET winver=%%a
+::for /f "tokens=1 delims=." %%a in ('ECHO %winver%') do SET winver=%%a
 
 :start
 CLS
-ECHO 1. URL         %url%
-ECHO 2. AUDIO/VIDEO %format%
-ECHO 3. CHECK
-ECHO 4. EXECUTE
-ECHO 5. UPDATE
-ECHO 6. EXIT
+ECHO 1. URL    %url%
+ECHO 2. FORMAT %format%
+ECHO 3. CUSTOMIZE
+ECHO 4. CHECK
+ECHO 5. EXECUTE
+ECHO 6. UPDATE
+ECHO 7. EXIT
 
 ECHO Select one option
-CHOICE /C 123456
-
-IF ERRORLEVEL 6 GOTO :opt_6
-IF ERRORLEVEL 5 GOTO :opt_5
-IF ERRORLEVEL 4 GOTO :opt_4
-IF ERRORLEVEL 3 GOTO :opt_3
-IF ERRORLEVEL 2 GOTO :opt_2
-IF ERRORLEVEL 1 GOTO :opt_1
+CHOICE /C 1234567
+GOTO opt_%ERRORLEVEL%
 
 :opt_1
 	ECHO.
 	SET /p url="Enter URL here: "
 	::escaping &
 	SET "url=%url:&=^&%"
-	GOTO :start
+	GOTO start
 
 :opt_2
 	ECHO.
 	ECHO Select V for VIDEO
 	ECHO Select A for AUDIO
-	CHOICE /C AV
-	IF ERRORLEVEL 2 GOTO :VIDEO
-	IF ERRORLEVEL 1 GOTO :AUDIO
-	:VIDEO
+	ECHO Select B to go back
+	CHOICE /C AVB
+	IF ERRORLEVEL 3 GOTO start
+	IF ERRORLEVEL 2 GOTO video
+	IF ERRORLEVEL 1 GOTO audio
+	:video
 		SET format=%video%
-		GOTO :start
-	:AUDIO
+		GOTO start
+	:audio
 		SET format=%audio%
-		GOTO :start
+		GOTO start
 
 :opt_3
 	ECHO.
-	ECHO  URL:    %url%
-	IF %format%==%video% ECHO  Format: (x)VIDEO ( )AUDIO
-	IF %format%==%audio% ECHO  Format: ( )VIDEO (x)AUDIO
-	ECHO.
-	ECHO Hit any key to back to options.
-	PAUSE >NUL
-	GOTO :start
+	ECHO Select C for custom parameters
+	ECHO Select H for help
+	ECHO Select B to go back
+	CHOICE /C CHB
+	IF ERRORLEVEL 3 GOTO start
+	IF ERRORLEVEL 2 GOTO help
+	IF ERRORLEVEL 1 GOTO custom
+	:custom
+		SET /P custom=Enter custom parameters:
+		SET format=%custom%
+		GOTO start
+	:help
+		ECHO Viewing youtube-dl_help.txt
+		IF EXIST youtube-dl_help.txt notepad youtube-dl_help.txt 
+		IF NOT EXIST youtube-dl_help.txt ECHO Take a look and close this file to contunue. >youtube-dl_help.txt & ECHO. >>youtube-dl_help.txt & youtube-dl -help >>youtube-dl_help.txt & notepad youtube-dl_help.txt
+		CLS
+		GOTO opt_3
 
 :opt_4
 	ECHO.
-	IF %format%==%video% GOTO :video
-	IF %format%==%audio% GOTO :audio
+	ECHO  URL:    %url%
+	IF %format%==%video% ECHO  Format: (x)VIDEO ( )AUDIO ( )CUSTOM
+	IF %format%==%audio% ECHO  Format: ( )VIDEO (x)AUDIO ( )CUSTOM
+	IF %format%==%custom% ECHO  Format: ( )VIDEO ( )AUDIO (x)CUSTOM: %custom%
+	ECHO.
+	ECHO Hit any key to back to options.
+	PAUSE >NUL
+	GOTO start
+
+:opt_5
+	ECHO.
+	IF %format%==%video% GOTO video
+	IF %format%==%audio% GOTO audio
+	IF %format%==%custom% GOTO custom
 	:video
 		::escaping "
 		SET format=%format:"=%
-		@youtube-dl.exe --no-overwrites --no-continue --restrict-filenames  %format% %url%
+		youtube-dl.exe %format% %url%
 		SET format=%video%
-		ECHO.
-		ECHO Done. Hit any key to back to options.
-		PAUSE >NUL
-		GOTO :start
+		GOTO finish
 	:audio
 		::escaping "
 		SET format=%format:"=%
-		@youtube-dl.exe --no-overwrites --no-continue --restrict-filenames  %format% %url%
+		@youtube-dl.exe %format% %url%
 		SET format=%audio%
+		GOTO finish
+	:custom
+		::escaping "
+		SET format=%format:"=%
+		@youtube-dl.exe %format% %url%
+		SET format=%custom%
+		GOTO finish
+	:finish	
 		ECHO.
 		ECHO Done. Hit any key to back to options.
 		PAUSE >NUL
-		GOTO :start
+		GOTO start
 
-:opt_5
+:opt_6
 	ECHO.
 	@youtube-dl.exe -U
 	ECHO.
 	ECHO Done. Hit any key to back to options.
 	PAUSE >NUL
-	GOTO :start
+	GOTO start
 
-:opt_6
+:opt_7
 	EXIT
